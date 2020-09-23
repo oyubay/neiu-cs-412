daa7e898d5224ea125d736a4aec117e8b3c66089
oyubay <70605768+oyubay@users.noreply.github.com>
2020-09-23 12:14:39 -0500

    Update statuschange.js

--- cargotracker/public/js/statuschange.js
+++ cargotracker/public/js/statuschange.js
@@ -2,5 +2,6 @@ let cargostatus = require('./cargostatus')
 exports.changeStatus= function (){
     let tempStatus=cargostatus.getStatus()
     console.log("You changed your status to " + tempStatus)
+    console.log("hello beauti")
     return true
 }
